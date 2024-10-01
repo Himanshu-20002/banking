6440ca8f6f08d366de846591244e95edc8d17891
Himanshu-20002 <134383327+Himanshu-20002@users.noreply.github.com>
2024-10-02 01:46:37 +0530

    second commit

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,27 @@
-
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
 export default function RootLayout({
     children,
   }: Readonly<{
     children: React.ReactNode;
   }>) {
+    const loggedIn={firstName:'john',lastName:'doe'};
+
+
     return (
-      <main>
-        SIDEBAR
+      <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
+        <div className="flex size-full flex-col ">
+          <div className="root-layout">
+            <Image src="/icons/logo.svg" alt="logo" width={32} height={32}/>
+            <div>
+              <MobileNav user={loggedIn}/>
+            </div>
+          </div>
+
         {children}
+        </div>
         </main>
   
     );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,34 @@
-import React from 'react'
-
+import React from "react";
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import RightSidebar from "@/components/RightSidebar";
 const Home = () => {
+  const loggedIn = { firstName: "Aderian", lastName: "Oke", email: "aderianoke@gmail.com" };
+  const sampleBanks: Bank[] = [
+    { $id: "1", name: "crista", balance: 1000 },
+    { $id: "2", name: "pikabo", balance: 2000 }
+  ];
   return (
-    <div>
-        home
-      
-    </div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage yout account  and transaction efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1255.55}
+          />
+        </header>
+       RecentTransactions
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={sampleBanks} />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
